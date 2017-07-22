4895c021d5523daefad3cc61624d06cc83cabe50
yanhaoqi <yanhaoqi2008@foxmail.com>
2017-07-22 21:34:48 +0800

    识别路由

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 /**
  * Created by yanhaoqi on 2017/7/22.
  */
+var server = require('./server.js')
+server.start();
--- server.js
+++ server.js
@@ -1,3 +1,20 @@
 /**
  * Created by yanhaoqi on 2017/7/21.
  */
+var http = require('http');
+var url = require('url');
+function start(){
+    http.createServer(function(request,response){
+        console.log('request recived 收到请求');
+        var pathname = url.parse(request.url).pathname;
+        if(pathname != '/favicon.ico'){
+            console.log('req url 请求路由',pathname);
+            response.setHeader('Content-type','text/html;charset = UTF-8');
+            // response.writeHead(200,{'Content-type':'text/plain'});
+            response.write('HELLO WORLD 闫浩奇');
+            response.end();
+        }
+    }).listen(8888);
+    console.log('server start')
+};
+exports.start = start
