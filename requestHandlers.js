3136ccc0c4ecd0a178cb1ac0046012426375931f
yanhaoqi <yanhaoqi2008@foxmail.com>
2017-07-24 12:48:35 +0800

    后台服务器生成前端页面

--- requestHandlers.js
+++ requestHandlers.js
@@ -4,13 +4,21 @@
 var exec = require('child_process').exec;
 function start(response){
     console.log("request handler 'start' was called");
-    exec('find /',function(error,stdout,stderr){
-        response.setHeader('Content-type','text/html;charset=UTF-8');
-        response.write(stdout);
-        response.end();
-        console.log('耗时操作 开始执行')
-    });
-    console.log('我是 耗时操作 后面的代码')
+    var body = '<html>'+
+        '<head>'+
+        '<meta http-equiv="Content-Type" content="text/html; '+
+        'charset=UTF-8" />'+
+        '</head>'+
+        '<body>'+
+        '<form action="/upload" method="post">'+
+        '<textarea name="text" rows="20" cols="60"></textarea>'+
+        '<input type="submit" value="Submit text" />'+
+        '</form>'+
+        '</body>'+
+        '</html>';
+    response.setHeader('content-type','text/html;charset=UTF-8');
+    response.write(body);
+    response.end();
 }
 function upload(response){
     console.log("request handler 'upload' was called");
